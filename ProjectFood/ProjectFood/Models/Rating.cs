2127ee704b35f6ef0307fb25f83690db56efdff5
Mathias Sass Michno <mathiassmichno@gmail.com>
2014-10-15 11:26:47 +0200

    Sidste klasser tilføjet modellaget
    måske kommer der flere (det gør der...)

---
+++ ProjectFood/ProjectFood/Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectFood.Models
+{
+    public class Rating
+    {
+        public readonly User User;
+        public decimal Rating { get; set; }
+        public readonly Recipe Recipe;
+    }
+}
---
+++ ProjectFood/ProjectFood/Models/Store.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectFood.Models
+{
+    class Store
+    {
+        public string Chain { get; set; }
+        public List<Offer> CurrentOffers { get; set; }
+        public List<Offer> NextOffers { get; set; }
+    }
+}
