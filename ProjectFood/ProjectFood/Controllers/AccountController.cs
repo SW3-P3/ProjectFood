94435d082157b0d045cc5199462a9a2d0dff04ba
Troels Kroegh <troels.kroegh@gmail.com>
2014-12-11 14:57:39 +0100

    Soren breakpoint

--- ProjectFood/ProjectFood/Controllers/AccountController.cs
+++ ProjectFood/ProjectFood/Controllers/AccountController.cs
@@ -176,7 +176,7 @@ namespace ProjectFood.Controllers
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                    ss
+                    // TODO: SØREN DU ER HER!   
                     var list = new ShoppingList();
                     list.Title = "Min Indkøbsliste";
                     var tmpUser = new User() {Username = model.Email, Name = (name.Contains(",") == true ? name.Split(',').First() : name)};
